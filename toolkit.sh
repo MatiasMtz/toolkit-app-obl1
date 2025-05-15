fde7d0c96302ae5baeeaf02b3a8ec17180e27aa6
MatiasMtz <matimartinez1000@gmail.com>
2025-05-15 01:17:57 +0000

    closeapp, default case added and routeRequest function

--- toolkit.sh
+++ toolkit.sh
@@ -34,40 +34,49 @@ showMenu() {
     return
 }
 
-showProperties() {
-    # Si la variable path no está vacía, se utiliza su valor para mostrar propiedades
-    if [ -n "$path" ]; then
-        echo
-        echo "*****************"
-        echo "PROPIEDADES DE LA CARPETA $path:"
-        echo "Número de archivos en la ruta: $(find $path -maxdepth 1 -type f | wc -l)"
-        echo "Número de archivos en subcarpetas: $(find $path -mindepth 2 -type f | wc -l)"
-        echo "Archivo de mayor tamaño: $(find $path -type f -exec du -b {} + | sort -nr | head -n 1)"
-        echo "Archivo de menor tamaño: $(find $path -type f -exec du -b {} + | sort -ns | head -n 1)"
-        echo "*****************"
-        echo
-        # agregar funcion para volver al menu principal
-        return
-    fi
-    # Si la variable path está vacía, se solicita al usuario que ingrese una ruta
+routeRequest() {
     read -p "Ingrese la ruta de la carpeta: " tempPath
     while [ ! -d "$tempPath" ]; do
         echo "La ruta no es válida. Intente nuevamente: "
         read -p "Ingrese la ruta de la carpeta: " tempPath
         echo
     done
+    echo $tempPath
+}
+
+showProperties() {
+    if [ -n "$path" ]; then
+        echo
+        echo "*****************"
+        echo "PROPIEDADES DE LA CARPETA $path:"
+        echo "Número de archivos en la ruta: $(find $path -maxdepth 1 -type f | wc -l)"
+        echo "Número de archivos en subcarpetas: $(find $path -mindepth 2 -type f | wc -l)"
+        # cambiar output para mostrar -> solo nombre de archivo en lugar de ruta completa
+        echo -e "Archivo de mayor tamaño | Peso en Bytes\n$(find $tempPath -type f -exec du -b {} + | sort -nr | head -n 1 | awk '{print $2, $1}')"
+        echo -e "Archivo de menor tamaño | Peso en Bytes\n$(find $tempPath -type f -exec du -b {} + | sort -n | head -n 1 | awk '{print $2, $1}')"
+        echo "*****************"
+        echo
+        return
+    fi
+    tempPath=$(routeRequest)
     echo
     echo "*****************"
     echo "PROPIEDADES DE LA CARPETA $tempPath"
-    echo "Número de archivos en la ruta: $(find $tempPath -maxdepth 1 -type f | wc -l)"
-    echo "Número de archivos en subcarpetas: $(find $tempPath -mindepth 2 -type f | wc -l)"
-    # cambiar output para mostrar -> Archivo de mayor tamaño: <nombre_archivo> <tamaño>
-    echo "Archivo de mayor tamaño: $(find $tempPath -type f -exec du -b {} + | sort -nr | head -n 1)"
-    echo "Archivo de menor tamaño: $(find $tempPath -type f -exec du -b {} + | sort -ns | head -n 1)"
+    echo "-- Número de archivos en la ruta: $(find $tempPath -maxdepth 1 -type f | wc -l)"
+    echo "-- Número de archivos en subcarpetas: $(find $tempPath -mindepth 2 -type f | wc -l)"
+    echo
+    # cambiar output para mostrar -> solo nombre de archivo en lugar de ruta completa
+    echo -e "-- Archivo de mayor tamaño | Peso en Bytes\n$(find $tempPath -type f -exec du -b {} + | sort -nr | head -n 1 | awk '{print $2, $1}')"
+    echo -e "-- Archivo de menor tamaño | Peso en Bytes\n$(find $tempPath -type f -exec du -b {} + | sort -n | head -n 1 | awk '{print $2, $1}')"
     echo "*****************"
     echo
-    # agregar funcion para volver al menu principal
     return
+}
+
+closeApp() {
+    echo "Saliendo de la aplicación..."
+    sleep 1
+    showToolkit=0
 }
 
 clear
@@ -98,9 +107,12 @@ while [ "$showToolkit" -eq 1 ]; do
 
             ;;
         8)
-
+            closeApp
             ;;
         *)
-            # establecer opcion else del switch
+            echo "*** Opción no reconocida. Intente nuevamente. ***"
+            sleep 2
+            clear
+            ;;
     esac
 done
